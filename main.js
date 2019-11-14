0e58525bff663f20ad5c97874b73b3a6dde092eb
fedPass <federica.passanante@gmail.com>
2019-11-14 16:24:50 +0100

    test su console e collegamento style

--- main.js
+++ main.js
@@ -2,20 +2,30 @@
 //alert('prova prova');
 
 //prezzo: 0,21 €/km
+var prezzo = 0.21;
 // km: prompt quanti km percorrerai?
-// anni: prompt quanti anni hai?
-// prezzoStandard = prezzo * km
-
-//A- anni < 18 --> prezzoScontato = (prezzoStandard - (prezzoStandard *20 / 100))
-
-//B- anni > 64 --> prezzoScontato = (prezzoStandard - (prezzoStandard * 40 / 100))
-
 var km = parseFloat(prompt('Quanti kilometri devi percorrere?'));
+// anni: prompt quanti anni hai?
 var anni = parseInt(prompt('Quanti anni hai?'));
-
+// controllo che siano numeri
 if (isNaN(anni&&km)) {
     console.log('Ooops...non hai digitato un numero!')
 } else {
     console.log('devi percorrere ' + km + ' km');
     console.log('la tua età è ' + anni + ' anni')
+}
+// calcolo il prezzo standard
+// prezzoStandard = prezzo * km
+var prezzoStandard = prezzo * km;
+console.log('Il prezzo del biglietto è: ' + prezzoStandard + '€');
+// calcolo se hanno lo sconto e il prezzo prezzoScontato
+//A- anni < 18 --> prezzoScontato = (prezzoStandard - (prezzoStandard *0.2))
+var prezzoJunior = parseFloat(prezzoStandard - (prezzoStandard * 0.2));
+//B- anni > 64 --> prezzoScontato = (prezzoStandard - (prezzoStandard * 0.4))
+var prezzoSenior = parseFloat(prezzoStandard - (prezzoStandard * 0.4));
+
+if (anni < 18) {
+    console.log('Il prezzo con sconto Junior è: ' + prezzoJunior + '€')
+} else if (anni > 64) {
+    console.log('Il prezzo con sconto Senior è: ' + prezzoSenior + '€')
 }
