22b959316bbdea1f56ee8c0ec2e669b87892a9a8
fedPass <federica.passanante@gmail.com>
2019-11-14 15:11:05 +0100

    collegamento file e steps da seguire

---
+++ main.js
@@ -0,0 +1,11 @@
+// prova collegamento file
+//alert('prova prova');
+
+//prezzo: 0,21 €/km
+// km: prompt quanti km percorrerai?
+// anni: prompt quanti anni hai?
+// prezzoStandard = prezzo * km
+
+//A- anni < 18 --> prezzoScontato = (prezzoStandard - (prezzoStandard *20 / 100))
+
+//B- anni > 64 --> prezzoScontato = (prezzoStandard - (prezzoStandard * 40 / 100))
